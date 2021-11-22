6f42526f441648db2177bc6c1e559f6a9af5a27a
Mads <mads61096@gmail.com>
2021-11-22 12:09:00 +0100

    Add project files.

---
+++ UDP raspberry pi modtager/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP_raspberry_pi_modtager
+{
+    class Program
+    {
+        // https://msdn.microsoft.com/en-us/library/tst0kwb1(v=vs.110).aspx
+        // IMPORTANT Windows firewall must be open on UDP port 7000
+        // https://www.windowscentral.com/how-open-port-windows-firewall
+        // Use the network MGV-xxx to capture from local IoT devices (fake or real)
+        private const int Port = 7000 ;
+        //private static readonly IPAddress IpAddress = IPAddress.Parse("192.168.5.137"); 
+        // Listen for activity on all network interfaces
+        // https://msdn.microsoft.com/en-us/library/system.net.ipaddress.ipv6any.aspx
+        static void Main()
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, Port);
+            using (UdpClient socket = new UdpClient(ipEndPoint))
+            {
+                IPEndPoint remoteEndPoint = new IPEndPoint(0, 0);
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast {0}", socket.Client.LocalEndPoint);
+                    byte[] datagramReceived = socket.Receive(ref remoteEndPoint);
+
+                    string message = Encoding.ASCII.GetString(datagramReceived, 0, datagramReceived.Length);
+                    Console.WriteLine("Receives {0} bytes from {1} port {2} message {3}", datagramReceived.Length,
+                        remoteEndPoint.Address, remoteEndPoint.Port, message);
+                    //Parse(message);
+                }
+            }
+        }
+
+        // To parse data from the IoT devices (depends on the protocol)
+        private static void Parse(string response)
+        {
+            string[] parts = response.Split(' ');
+            foreach (string part in parts)
+            {
+                Console.WriteLine(part);
+            }
+            string temperatureLine = parts[6];
+            string temperatureStr = temperatureLine.Substring(temperatureLine.IndexOf(": ") + 2);
+            Console.WriteLine(temperatureStr);
+        }
+    }
+}
